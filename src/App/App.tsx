c3f3347b1714b84ce796b30d8e7672d2486e4223
Aymen Mensi <v-aymenmensi@microsoft.com>
2023-03-08 11:44:37 +0100

    adding sass and new file structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "../logo.svg";
 
-function App() {
+import "./App.scss";
+
+const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
